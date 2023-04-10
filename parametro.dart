adb987e9eea6912d383eb5e10f021c529490f557
GUSTAVO DE OLIVEIRA FERREIRA <gustavo.6939@aluno.pr.senac.br>
2023-04-10 10:33:21 -0300

    pukemon

---
+++ parametro.dart
@@ -0,0 +1,9 @@
+void main(){
+  first("Blazeken", "lutador", "fogo");
+  first("Raichu de Alola", "elétrico", "psiquico");
+  first("Pigeot", "voador", "normal");
+}
+
+void first(String pokemon, tipo_um, tipo_dois){
+  print("$pokemon é um pokemon do tipo $tipo_um e $tipo_dois");
+}
